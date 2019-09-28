b09297b7e05f1526f420b3cf7656138624d5d139
liu1935913 <132422664@qq.com>
2019-09-28 15:58:45 +0800

    11

---
+++ lianxi/src/lianxi/lianxi.java
@@ -0,0 +1,12 @@
+package lianxi;
+
+public class lianxi {
+
+	public static void main(String[] args) {
+		System.out.println("nihao");
+		System.out.println("nihao");
+		System.out.println("nihao");
+
+	}
+
+}
