ad4e0130e8c970928e95fce0aded76e2cbbdfc55
liu1935913 <132422664@qq.com>
2019-10-08 20:32:54 +0800

    已修改

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -4,7 +4,12 @@ public class test1 {
 
 	public static void main(String[] args) {
 	System.out.println("11111111");
-			
+	System.out.println("远程修改");		
+	System.out.println("远程在线修改");		
+	System.out.println("远程在线修改");		
+	System.out.println("远程在线修改");		
+	System.out.println("远程在线修改");		
+
 		
 
 	}
