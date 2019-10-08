6d97a05d0d5bc4d5457b580f6b1298e7937be24e
liu1935913 <132422664@qq.com>
2019-10-08 19:32:07 +0800

    添加提交

---
+++ lpl1/src/lpl1/test2.java
@@ -0,0 +1,17 @@
+package lpl1;
+
+public class test2 {
+
+	public static void main(String[] args) {
+	System.out.println("11111111");
+	System.out.println("远程修改");		
+	System.out.println("本地修改");
+	System.out.println("冲突解决2");
+	System.out.println("冲突解决2");
+	System.out.println("冲突解决2");
+	System.out.println("冲突解决2");
+	
+
+	}
+
+}
