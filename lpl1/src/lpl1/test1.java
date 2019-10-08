3acf83e03e38779fb6493a1cea8b34d7d058a449
liu1935913 <50741203+liu1935913@users.noreply.github.com>
2019-10-08 19:49:54 +0800

    Update test1.java

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -6,7 +6,7 @@ public class test1 {
 	System.out.println("11111111");
 	System.out.println("远程修改");		
 	System.out.println("本地修改");
-	System.out.println("本地修改");
+	System.out.println("会有冲突");
 		
 
 	}
