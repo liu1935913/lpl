3df1ebf76ad5f340dececab502605c61fbafca48
liu1935913 <132422664@qq.com>
2019-10-08 19:26:40 +0800

    冲突解决

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -4,8 +4,8 @@ public class test1 {
 
 	public static void main(String[] args) {
 	System.out.println("11111111");
-	System.out.println("远程修改");
-		
+	System.out.println("远程修改");		
+	System.out.println("本地修改");
 
 	}
 
