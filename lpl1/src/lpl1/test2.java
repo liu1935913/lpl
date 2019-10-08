0976bdd3dc5a6c527603c1ba4a32d79db3723d2a
liu1935913 <50741203+liu1935913@users.noreply.github.com>
2019-10-08 20:03:19 +0800

    Update test2.java

--- lpl1/src/lpl1/test2.java
+++ lpl1/src/lpl1/test2.java
@@ -4,9 +4,7 @@ public class test2 {
 
 	public static void main(String[] args) {
 	System.out.println("11111111");
-	System.out.println("远程修改");		
-	System.out.println("本地修改");
-	System.out.println("你猜");
+
 	
 
 	}
