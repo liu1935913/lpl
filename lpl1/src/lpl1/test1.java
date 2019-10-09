8ed01b40bfdcdee5f4fe05389febf928eff93902
liu1935913 <132422664@qq.com>
2019-10-09 08:46:09 +0800

    tijoa

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -1,12 +1,13 @@
 package lpl1;
 
 public class test1 {
-
+	String b="5";
 	public static void main(String[] args) {
 		
 	System.out.println("11111111111");
 		System.out.println("222222222");
 		
+
 
 	
 
