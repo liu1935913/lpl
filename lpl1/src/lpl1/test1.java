e368f11dfac30a1465d0fbc40031a5aa1339119f
liu1935913 <132422664@qq.com>
2019-10-09 17:16:08 +0800

    111

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -1,18 +1,15 @@
 package lpl1;
 
 public class test1 {
-	
-	public static void main(String[] args) {		
-	System.out.println("开始1");
-		
-		
-
 
 	
-
-
-		
-
+	public static void main(String[] args) throws Exception {
+		Integer len=10;
+		byte[] a=new byte[len];
+		String rst=new String(a,"utf-8");
+		System.out.println(rst);
 	}
+	
+	
 
 }
