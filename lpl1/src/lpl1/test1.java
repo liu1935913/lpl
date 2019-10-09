5cf8b6e05fef009d6f2ce2c3d994b79dc0c18c6a
liu1935913 <50741203+liu1935913@users.noreply.github.com>
2019-10-09 08:42:03 +0800

    Update test1.java

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -3,7 +3,10 @@ package lpl1;
 public class test1 {
 
 	public static void main(String[] args) {
-	System.out.println("222222");
+		
+	System.out.println("11111111111");
+		System.out.println("222222222");
+		
 
 	
 
