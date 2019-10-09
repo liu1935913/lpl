f6a8fa9a028a6ae24561f19acd8d33bff495277d
liu1935913 <50741203+liu1935913@users.noreply.github.com>
2019-10-09 17:07:43 +0800

    Update test1.java

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -2,10 +2,8 @@ package lpl1;
 
 public class test1 {
 	
-	public static void main(String[] args) {
-		int a=5;
-		int b=6;
-	System.out.println("11111111111");
+	public static void main(String[] args) {		
+	System.out.println("开始1");
 		
 		
 
