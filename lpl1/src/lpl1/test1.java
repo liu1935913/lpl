56b72313bbea1b855dad610bb31da3ef0bda43d2
liu1935913 <132422664@qq.com>
2019-10-09 17:52:10 +0800

    lpl

--- lpl1/src/lpl1/test1.java
+++ lpl1/src/lpl1/test1.java
@@ -5,6 +5,7 @@ public class test1 {
 	
 	public static void main(String[] args) throws Exception {
 		Integer len=10;
+		int b=9;
 		byte[] a=new byte[len];
 		String rst=new String(a,"utf-8");
 		System.out.println(rst);
